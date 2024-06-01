4f7b285e149bca0974ff8e8482ac805e857118b2
ETAMSETTIGANGAHEMANTH <103168797+ETAMSETTIGANGAHEMANTH@users.noreply.github.com>
2024-06-01 14:05:20 +0530

    Update server.js

--- src/server.js
+++ src/server.js
@@ -4,13 +4,14 @@ const cors = require("cors");
 const app = express();
 
 app.use(cors({
-  origin: 'codejava-app-10-frontend-1ef5c823fa33.herokuapp.com',
+  origin: 'https://codejava-app-10-frontend-1ef5c823fa33.herokuapp.com',
 }));
 app.get("/", (req,res)=>{
 res.send("server working");
 });
 
-app.listen(8080, ()=>{
-console.log("Server Listen on http://localhost:8080");
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
 });
  
